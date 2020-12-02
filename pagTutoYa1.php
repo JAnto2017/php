930c4987a16f03ebc4cf928f171e1beda0fabd87
JAnto2017 <cursooperaciones2017@gmail.com>
2020-12-02 23:05:52 +0000

    Página 1 con formulario - Tutoriales de programación Ya

---
+++ pagTutoYa1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Captura datos del formulario</title>
+    </head>
+    <body>
+        <?php
+        echo "El nombre ingresado es: ";
+        echo $_REQUEST['nombre'];
+        ?>
+    </body>
+</html>
